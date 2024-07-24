283df85b0b01b2ede29fa474688622564778a1f2
devLopesRenato <renatolopes37@gmail.com>
2024-07-23 23:10:03 -0300

    desafio poo -Bootcamp Backend com Java

--- poo/src/Main.java
+++ poo/src/Main.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Curso crusoJava = new Curso();
+        crusoJava.setTitulo("Curso java");
+        crusoJava.setDescricao("Descrição do curso de java");
+        crusoJava.setCargaHoraria(15);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Curso crusoJavaScript = new Curso();
+        crusoJavaScript.setTitulo("Curso JavaScript");
+        crusoJavaScript.setDescricao("Descrição do curso de JavaScript");
+        crusoJavaScript.setCargaHoraria(10);
+
+        Mentoria mentoriaJava = new Mentoria();
+        mentoriaJava.setTitulo("Mentoria de Java");
+        mentoriaJava.setDescricao("Descrição da mentoria de Java");
+        mentoriaJava.setData(LocalDate.now());
+
+        System.out.println(crusoJava);
+        System.out.println(crusoJavaScript);
+        System.out.println(mentoriaJava);
+
+        Bootcamp bootcampJavaDev = new Bootcamp();
+        bootcampJavaDev.setNome("Bootcamp Dio Java Developer");
+        bootcampJavaDev.setDescricao("Descrição Bootcamp Dio Java Developer");
+        bootcampJavaDev.getConteudos().add(crusoJava);
+        bootcampJavaDev.getConteudos().add(crusoJavaScript);
+        bootcampJavaDev.getConteudos().add(mentoriaJava);
+
+        System.out.println("#####################");
+
+        Dev devRenato = new Dev();
+        devRenato.setNome("Renato");
+        devRenato.inscreverBootcamp(bootcampJavaDev);
+        System.out.println("Conteúdos Inscritos Renato:" + devRenato.getConteudosInscritos());
+        devRenato.progredir();
+        devRenato.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Renato:" + devRenato.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Renato:" + devRenato.getConteudosConcluidos());
+        System.out.println("XP:" + devRenato.calcularTotalXp());
+
+        System.out.println("#####################");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcampJavaDev);
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos João:" + devJoao.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcularTotalXp());
+
     }
+
 }
